1f66edbb2dbf4c0e2fbcab196fe1ec10e7877866
Michael Peterson <michaeljoepeterson@gmail.com>
2019-12-08 12:23:13 -0700

    initial commit with initial hero class

---
+++ heroApi/Models/Hero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace heroApi.Models
+{
+    public class Hero
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public static List<Hero> HeroList = new List<Hero>();
+        private static int maxId = 0;
+        
+
+        public Hero(int _id,string _name)
+        {
+            id = _id;
+            name = _name;
+            if(_id > maxId)
+            {
+                maxId = _id;
+            }
+            HeroList.Add(this);
+        }
+
+        public Hero(string _name)
+        {
+            name = _name;
+            id = checkMaxID();
+            HeroList.Add(this);
+        }
+        //check if max id taken if id not passed
+        private int checkMaxID()
+        {
+            for(int i = 0;i < HeroList.Count; i++)
+            {
+                if (HeroList[i].id == maxId)
+                {
+                    maxId++;
+                    return maxId;
+                }
+            }
+
+            return maxId;
+        }
+    }
+}
